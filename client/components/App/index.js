5c4801e47f31f04cd850239e135a87b306348ce7
Nghia <nghia.phan.gg@gmail.com>
2022-11-30 15:55:04 +0700

    first commit

---
+++ client/components/App/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from './style.less'
+
+const App = () => (<>
+    <div className={style.myapp}>Hello World</div>
+    {/* <button onClick={e => alert('Hello You!')}>Say Hello Back!</button> */}
+</>);
+
+export default App; 
---
+++ webpack.shared.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    mode: process.env.NODE_ENV === 'production' 
+        ? 'production' 
+        : 'development',
+
+    module: {
+        rules: [
+            { 
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use:  'css-loader', 
+              }
+        ],
+    },
+
+    resolve : {
+        extensions : ['.js', '.jsx' , '.css', '.less']
+    },
+};
