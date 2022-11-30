2ade3be04f308107f47957205d95bd118a2089ee
Nghia <nghia.phan.gg@gmail.com>
2022-11-30 16:08:30 +0700

    Q1,Setup SSR with React

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -3,7 +3,6 @@ import style from './style.less'
 
 const App = () => (<>
     <div className={style.myapp}>Hello World</div>
-    {/* <button onClick={e => alert('Hello You!')}>Say Hello Back!</button> */}
 </>);
 
 export default App; 
